389537815a47f1ca7db38ed9873d86899eefffff
Niticon Davis <niticon.davis@gmail.com>
2020-09-16 15:24:27 -0700

    you know I had to do it to em

--- Word2Vec/implement_Word2Vec.py
+++ Word2Vec/implement_Word2Vec.py
@@ -1,2 +1,19 @@
 import word2vec
+import pathlib
+import argparse
 
+def loop_dir(directory):
+	directory += '/'
+	path = pathlib.Path(directory)
+	for fl in path.iterdir():
+		with open(fl, 'r') as f:
+			print("Processing file\t" + fl)
+			for byts in f:
+				print(byts)
+			f.close()
+	return f
+
+if __name__ == "__main__":
+#file_directory=open(args.path, "r").read()
+	file_directory=args.path
+	loop_dir(file_directory)
